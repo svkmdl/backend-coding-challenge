c9c62f9667c3ac7263492028f8e97dba0eea2a4c
svkmdl <souvikmondal15@gmail.com>
2025-07-27 21:13:34 +0200

    feat: unit test for util function gists_for_user

--- tests/test_gistapi.py
+++ tests/test_gistapi.py
@@ -1,9 +1,18 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from gistapi.gistapi import gists_for_user
 
-# Unit tests
+# Unit tests (used ref : https://medium.com/@moraneus/the-art-of-mocking-in-python-a-comprehensive-guide-8b619529458f)
 class TestUtils(unittest.TestCase):
-
-    def test_gists_for_user(self):
-        pass
+    @patch('gistapi.gistapi.requests.get')
+    def test_gists_for_user(self, mock_get: patch) -> None :
+        mock_response = mock_get.return_value
+        expected_data = {"foo" : "bar"}
+        mock_response.json.return_value = expected_data
+        username = 'justdionysus'
+        expected_url = 'https://api.github.com/users/justdionysus/gists'
+        actual_data = gists_for_user(username)
+        self.assertEqual(actual_data, expected_data)
+        mock_get.assert_called_once_with(expected_url)
+if __name__ == '__main__':
+    unittest.main()
