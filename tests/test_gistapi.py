1a5067554e83954b2616f65738cdccec618e94ed
svkmdl <souvikmondal15@gmail.com>
2025-07-27 14:30:27 +0200

    chore : added unit test boilerplate

---
+++ tests/test_gistapi.py
@@ -0,0 +1,9 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from gistapi.gistapi import gists_for_user
+
+# Unit tests
+class TestUtils(unittest.TestCase):
+
+    def test_gists_for_user(self):
+        pass
