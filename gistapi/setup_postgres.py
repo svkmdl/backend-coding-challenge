4479431ad128284277b58514e981792b61597911
svkmdl <souvikmondal15@gmail.com>
2025-11-30 16:15:56 +0100

    chore: setup postgres tables

--- gistapi/setup_postgres.py
+++ gistapi/setup_postgres.py
@@ -10,5 +10,34 @@ with conn.cursor() as curs:
     try:
         curs.execute("SELECT version()")
         print("You are connected to - ", curs.fetchone()[0])
+
+        create_users_table = """
+        CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        login TEXT NOT NULL
+        );
+        """
+        curs.execute(create_users_table)
+        print("Created users table")
+
+        create_gists_table = """
+        CREATE TABLE IF NOT EXISTS gists (
+        id UUID PRIMARY KEY,
+        user_id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+        gist_url TEXT NOT NULL,
+        content TEXT
+        );
+        """
+        curs.execute(create_gists_table)
+        print("Created gists table")
+
+        conn.commit()
+        print("Tables created successfully")
+
     except (Exception, psycopg2.DatabaseError) as error:
+        conn.rollback()
         print(error)
+
+    finally:
+        curs.close()
+        conn.close()
